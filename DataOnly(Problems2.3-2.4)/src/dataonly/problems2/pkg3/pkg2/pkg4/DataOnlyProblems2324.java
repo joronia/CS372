f2f6995486dedbcde940e7e9848d8183df85fa6f
joronia <jaoronia@gmail.com>
2015-01-05 16:05:45 -0800

    PR1_1: Data Only (Problems 2.3 & 2.4)
    Finished Code

---
+++ DataOnly(Problems2.3-2.4)/src/dataonly/problems2/pkg3/pkg2/pkg4/DataOnlyProblems2324.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package dataonly.problems2.pkg3.pkg2.pkg4;
+
+/**
+ *
+ * @author joronia17
+ */
+public class DataOnlyProblems2324 {
+int i;
+float f;
+boolean b;
+    public static void main(String[] args) {
+        DataOnlyProblems2324 d = new DataOnlyProblems2324();  // TODO code application logic here
+        d.i = 47;
+        d.f = 1.1f;
+        d.b = false;
+        System.out.println(d.i);
+        System.out.println(d.f);
+        System.out.println(d.b);
+        
+    }
+    
+}
