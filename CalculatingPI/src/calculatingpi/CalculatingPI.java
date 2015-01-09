4b9f10951454180a10ea5ae3bc4f282d473d6296
joronia <jaoronia@gmail.com>
2015-01-08 20:04:18 -0800

    GUI for Temperature Converter

---
+++ CalculatingPI/src/calculatingpi/CalculatingPI.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package calculatingpi;
+
+/**
+ *
+ * @author joronia17
+ */
+public class CalculatingPI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Integer i = Integer.parseInt(args[0]);
+//        
+//        System.out.println(i.toString()); 
+        if (args.length > 0) {
+            CalcPI human = new CalcPI(); 
+        human.calculatepi(i);
+        }
+        
+    }
+    
+}
