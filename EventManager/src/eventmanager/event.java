5a4c4c60fdc9ea21a0d4ee3a8ccf4e5b7b78ad6a
joronia <jaoronia@gmail.com>
2015-01-16 20:35:19 -0800

    PR3_3: Event Manager
    Finished

--- EventManager/src/eventmanager/event.java
+++ EventManager/src/eventmanager/event.java
@@ -9,18 +9,27 @@ import java.util.ArrayList;
 import java.util.Comparator;
 
 /**
- *
+ * Clasclass to set everything that is used for the event.
  * @author joronia17
  */
 public class event {
-
+// Initializing variables.
     private String _name, _location, _month, _date, _year;
     public ArrayList<String> values = new ArrayList();
-
+// An extra empty constructor.
     public event() {
 
     }
-
+    /**
+     * Method to set the name,location, month, date, and year.
+     * @param name
+     * @param location
+     * @param month
+     * @param date
+     * @param year 
+     */
+// Used to read the text from GUI and and adding them into an arrayList that
+ // has my texts from the functions.
     public event(String name, String location, String month, String date, String year) {
         //setname(name);
         values.add(setname(name));
@@ -33,12 +42,20 @@ public class event {
         //setyear(year);
         values.add(setyear(year));
     }
-
+    /**
+     * Method to return the arraylist of values. 
+     * @return values
+     */
+// return the the whole array of values.
     public ArrayList<String> getValues() {
 
         return values;
     }
-
+/**
+ * Parameter is an array of strings. Whenever an array is passed it adds v to values and then gets the index
+ * location for each property and assigns them to their proper variable.
+ * @param v 
+ */
     public void setValues(ArrayList<String> v) {
         values = v;
         _name = v.get(0);
@@ -47,7 +64,12 @@ public class event {
         _date = v.get(3);
         _year = v.get(4);
     }
-
+/**
+ * Setting the name. Checks to see if the array is greater than zero and see's if the other conditions are
+ * satisfied. If it is true then it gets the name and assigns it to index 0 of ArrayList.
+ * @param name
+ * @return 
+ */
     public String setname(String name) {
         _name = name;
         if (values.size() > 0 && !values.get(0).equals(name) && values.size() < 6) {
@@ -56,7 +78,11 @@ public class event {
 
         return name;
     }
-
+/**
+ * Same as above its to set and tells the compiler that there can not be duplicate elements. 
+ * @param location
+ * @return location
+ */
     public String setlocation(String location) {
         _location = location;
         if (values.size() > 1 && !values.get(1).equals(location) && values.size() < 6) {
@@ -64,7 +90,11 @@ public class event {
         }
         return location;
     }
-
+/**
+ * Same as the first one.
+ * @param month
+ * @return month
+ */
     public String setmonth(String month) {
         _month = month;
         if (values.size() > 2 && !values.get(2).equals(month) && values.size() < 6) {
@@ -72,7 +102,12 @@ public class event {
         }
         return month;
     }
-
+/**
+ * Same as the first one but only difference is that it has a try which catches the user when they try to
+ * add a string and not a integer. 
+ * @param date
+ * @return date
+ */
     public String setdate(String date) {
         _date = date;
         if (values.size() > 3 && !values.get(3).equals(date) && values.size() < 6) {
@@ -89,7 +124,11 @@ public class event {
 
         return date;
     }
-
+/**
+ * Same as the first one. 
+ * @param year
+ * @return year
+ */
     public String setyear(String year) {
         _year = year;
         if (values.size() > 4 && !values.get(4).equals(year) && values.size() < 6) {
@@ -97,30 +136,54 @@ public class event {
         }
         return year;
     }
-
+/**
+ * Return a value
+ * @return _name
+ */
     public String getname() {
         return _name;
     }
-
+/**
+ * return a value
+ * @return _location
+ */
     public String getlocation() {
         return _location;
     }
-
+/**
+ * return a value.
+ * @return _month
+ */
     public String getmonth() {
         return _month;
     }
-
+/**
+ * Returns a value
+ * @return _date
+ * @throws IllegalArgumentException 
+ */
     public String getdate() throws IllegalArgumentException {
         return _date;
     }
-
+/**
+ * returns the value of _year.
+ * @return _year
+ */
     public String getyear() {
         return _year;
     }
-
+/**
+ * overwrites the code to output something different like this toString Method. Shows in the list.
+ * @return String
+ */
     public String toString() {
         return "Event: " + _name + "|" + _location + "|" + _month + "|" + _date + "|" + _year;
     }
+    /**
+     * Comparator to sort the list by event object. Gets the location and compares it to the second and that is how
+     * it gets sorted. I figured it out why I could not sort on my GUI. I needed to clear the array of event and read
+     * the file again and then sort that new list and update the list that is shown.
+     */
      public static Comparator<event> LocationCompartor = new Comparator<event>() {
             
         @Override
@@ -131,7 +194,10 @@ public class event {
             
         }
    };
-     
+     /**
+      * Comparator to sort the list by event object. Gets the location and compares it to the second and that is how
+     * it gets sorted. This is for Dates. 
+      */
      public static Comparator<event> DateComparator = new Comparator<event>() {
 
         @Override
@@ -141,7 +207,10 @@ public class event {
         }
          
      };
-     
+     /**
+      * Comparator to sort the list by event object. Gets the location and compares it to the second and that is how
+     * it gets sorted. This is for Name.
+      */
      public static Comparator<event> NameComparator = new Comparator<event>() {
 
         @Override
--- EventManager/src/eventmanager/eventcalander.java
+++ EventManager/src/eventmanager/eventcalander.java
@@ -13,25 +13,39 @@ import java.io.FileWriter;
 import java.util.*;
 
 /**
- *
+ * Class to to do all writing to file and etc. 
  * @author joronia17
  */
 public class eventcalander {
 
     ArrayList<event> Events = new ArrayList();
     String f = "Events.txt";
-
+/**
+ * return the size of array event
+ * @return 
+ */
     public int getSize() {
         return Events.size();
     }
-
+/**
+ * Get the element at certain index.
+ * @param i
+ * @return 
+ */
     public event getElement(int i) {
         return Events.get(i);
     }
+    /**
+     * Return the vector of Events.
+     * @return 
+     */
     public ArrayList<event> getEvents(){
         return Events;
     }
-
+/**
+ * Add all event properties into vector of event.
+ * @param c 
+ */
     public void addevents(event c) {
 //       Events.add(_name);
 //       Events.add(_location);
@@ -42,12 +56,27 @@ public class eventcalander {
         Events.add(c);
 
     }
+    /**
+     * Function to tell the compiler that I want to sort properties of the array events that is from event.
+     */
 public void locationsort() {
     Events.sort(event.LocationCompartor);
 } 
+/**
+ * Same as above accept its for date.
+ */
 public void datesort() {
     Events.sort(event.DateComparator);
 }
+/**
+ * Same as above but for the name
+ */
+public void namesort() {
+    Events.sort(event.NameComparator);
+}
+/**
+ * Method to write to file all the properties when I click submit.
+ */
     public void addtofile() {
         try {
             //File f = new File("C:\\Users\\joronia17\\Documents\\GitHub\\CS372\\EventManager\\Events.txt");
@@ -63,7 +92,10 @@ public void datesort() {
             System.out.println("Looks like we are having some issues here!");
         }
     }
-
+/**
+ * Reads from file and returns ArrayList.
+ * @return Events
+ */
     public ArrayList<event> readfromfile() {
         try {
             File F = new File("Events.txt");
@@ -79,7 +111,11 @@ public void datesort() {
         }
         return Events;
     }
-    
+    /**
+     * A To String method. When I call it in the GUI.
+     * @param e
+     * @return 
+     */
     public String toString(event e){
         String line="";
         for(String s1:e.getValues()){
